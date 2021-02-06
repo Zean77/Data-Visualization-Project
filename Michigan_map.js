57114cc79a9e71427a1ec7b9bdb6f4728715a598
Gretchen A <and01487@umn.edu>
2021-02-05 19:09:14 -0600

    Load csv correct

--- Michigan_map.js
+++ Michigan_map.js
@@ -1,40 +1,23 @@
 d3.csv("Data/Data/Michigan_Covid_Vaccine_Data.csv").then(function(mi_data) {
-
-  //console.log(mi_data);
+ 
   mi_data.forEach(function(data) {
-    data.county =data["Person's Residence in County"];
-    data.preparedness = data["Person's Residence in Preparedness Region"];
-    data.jurisdiction = data["Person's Residence in Local Health Department Jurisdiction"];
-    data.vaccine= data["Vaccine Type"];
+    data.year_range = data["Year Range"];
+    data.preparedness = data["Persons Residence in Preparedness Region"];
+    data.jurisdiction = data["Persons Residence in Local Health Department Jurisdiction"];
+    data.county =data["Persons Residence in County"];
+    data.vaccine = data["Vaccine Type"];
+    data.facility = data["Facility Type"];
+    data.dose = data["Dose Number"];
     data.date=(data["Date"]);
     data.total_doses=+data["Number of Doses"];
-    data.dose_num=+data["Dose Number"];
-    
-    console.log(data.date);
+    console.log("County:",data.county);
+
 
 });  
    
 });
 
-  // Create function for dropdown menu
 
-   var menu=d3.select("#selDataset");
-   d3.csv("Data/Data/Michigan_Covid_Vaccine_Data.csv").then(function(mi_data) {
-
-    //console.log(mi_data);
-    mi_data.forEach(function(data) {
-      data.county =data["Person's Residence in County"];
-      
-      console.log(data.date);
-  
-  });  
-     
-  });
-      //console.log(counties);
-     
-     //menu.append("option").text(name);
-    });
-    
 
     function optionChanged(id) {
      
